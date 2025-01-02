487868a589dcb0c3e64bb8f83272137c678a312b
jus1d <artfa63@gmail.com>
2025-01-02 09:19:20 +0400

    chore: Add pqmigration

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE contests (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(64) NOT NULL,
+    description VARCHAR(512) DEFAULT '' NOT NULL,
+    creator_address VARCHAR(64) NOT NULL,
+    start TIMESTAMP NOT NULL,
+    duration INTERVAL NOT NULL,
+    max_slots INTEGER NOT NULL,
+    -- applied_participants INTEGER DEFAULT 0 NOT NULL,
+    is_draft BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT now() NOT NULL
+);
+
+CREATE TABLE problems
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(64) NOT NULL,
+    description VARCHAR(512) DEFAULT '' NOT NULL,
+    writer_address VARCHAR(64) NOT NULL,
+    -- kind ? -- TODO: maybe create an enum for problem kind like (single-answer, multi-answer, code)
+    -- difficulty ???
+    input TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT now() NOT NULL
+);
