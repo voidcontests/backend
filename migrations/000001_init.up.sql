65f7845921687be23cb3be93b7f0dfc9e9f41af7
jus1d <artfa63@gmail.com>
2025-01-02 09:28:11 +0400

    migrations: Add problemset

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,25 +1,3 @@
-CREATE TABLE contests (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(64) NOT NULL,
-    description VARCHAR(512) DEFAULT '' NOT NULL,
-    creator_address VARCHAR(64) NOT NULL,
-    start TIMESTAMP NOT NULL,
-    duration INTERVAL NOT NULL,
-    max_slots INTEGER NOT NULL,
-    -- applied_participants INTEGER DEFAULT 0 NOT NULL,
-    is_draft BOOLEAN NOT NULL,
-    created_at TIMESTAMP DEFAULT now() NOT NULL
-);
+DROP TABLE contests;
 
-CREATE TABLE problems
-(
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(64) NOT NULL,
-    description VARCHAR(512) DEFAULT '' NOT NULL,
-    writer_address VARCHAR(64) NOT NULL,
-    -- kind ? -- TODO: maybe create an enum for problem kind like (single-answer, multi-answer, code)
-    -- difficulty ???
-    input TEXT NOT NULL,
-    answer TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT now() NOT NULL
-);
+DROP TABLE problems;
