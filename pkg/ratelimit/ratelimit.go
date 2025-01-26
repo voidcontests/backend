c8dd1c1c2350c4807fe1bbb34953ee9ab972b49a
jus1d <artfa63@gmail.com>
2025-01-26 08:12:54 +0400

    fix: rate limiting

--- pkg/ratelimit/ratelimit.go
+++ pkg/ratelimit/ratelimit.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -11,17 +12,22 @@ import (
 
 // WithTimeout returns a middleware that allows only one request per `duration` from the same IP.
 func WithTimeout(duration time.Duration) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		lastRequests := make(map[string]time.Time)
+	lastRequests := make(map[string]time.Time)
+	var mu sync.Mutex
 
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			ip := c.RealIP()
+
+			mu.Lock()
+			defer mu.Unlock()
+
 			if lastRequest, ok := lastRequests[ip]; ok {
 				since := time.Since(lastRequest)
 				if since < duration {
 					slog.Debug("rate limited", slog.String("ip", ip))
 					return c.JSON(http.StatusTooManyRequests, map[string]any{
-						"timeout": fmt.Sprintf("%ds", int64((duration - since).Seconds())),
+						"timeout": fmt.Sprintf("%ds", int64(duration.Seconds()-since.Seconds())),
 					})
 				}
 			}
