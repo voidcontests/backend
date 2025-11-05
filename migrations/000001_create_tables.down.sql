33a5bcee0be1a5e16c7c073e880a7d62e1524f5c
jus1d <artfa63@gmail.com>
2025-11-05 16:26:59 +0400

    migrations: split creating view out of tables

--- migrations/000001_create_tables.down.sql
+++ migrations/000001_create_tables.down.sql
@@ -1,9 +1,3 @@
-DROP VIEW IF EXISTS contest_problemsets;
-DROP VIEW IF EXISTS problem_statuses;
-DROP VIEW IF EXISTS problem_details;
-DROP VIEW IF EXISTS contest_details;
-DROP VIEW IF EXISTS leaderboard;
-
 DROP TABLE IF EXISTS testing_reports;
 DROP TABLE IF EXISTS submissions;
 DROP TABLE IF EXISTS entries;
---
+++ migrations/000002_create_views.down.sql
@@ -0,0 +1,5 @@
+DROP VIEW IF EXISTS contest_problemsets;
+DROP VIEW IF EXISTS problem_statuses;
+DROP VIEW IF EXISTS problem_details;
+DROP VIEW IF EXISTS contest_details;
+DROP VIEW IF EXISTS leaderboard;
