2866e5bc3d36724655207b2afd495dffdfacc760
jus1d <artfa63@gmail.com>
2025-11-08 03:02:25 +0400

    chore: remove materialized views

--- migrations/000002_views.down.sql
+++ migrations/000002_views.down.sql
@@ -2,6 +2,5 @@ DROP VIEW IF EXISTS submissions_view;
 DROP VIEW IF EXISTS contest_problems_view;
 DROP VIEW IF EXISTS problem_statuses;
 DROP VIEW IF EXISTS problems_view;
-
-DROP MATERIALIZED VIEW IF EXISTS contests_view;
-DROP MATERIALIZED VIEW IF EXISTS scores;
+DROP VIEW IF EXISTS contests_view;
+DROP VIEW IF EXISTS scores;
